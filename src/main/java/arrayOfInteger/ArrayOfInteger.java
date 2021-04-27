66f48162107f11eff7cf404ad544396cff28cf91
deepmud <73426734+deepmud@users.noreply.github.com>
2021-04-27 17:40:43 +0100

    scanner is now working, you can now put your own number

--- src/main/java/arrayOfInteger/ArrayOfInteger.java
+++ src/main/java/arrayOfInteger/ArrayOfInteger.java
@@ -6,18 +6,11 @@ import java.util.Scanner;
 public class ArrayOfInteger {
 
     public int returnMinimumMissingInteger(){
+        int b = 8;
         int[] n = {8,1,2,7,3,5,6,10};
         int[] s = new int[8];
         Arrays.sort(n);
-        for(int i=0;i < 8;i++) {
-            s[i] = i + 1;
-            if(n[i]!=s[i]){
-                System.out.println(s[i]);
-                return s[i];
-            }
-        }
-        int m = 77;
-        return m;
+        return CheckMissingInteger( b, s, n);
     }
 
     public void returnMinimumMissingIntegerInUserTestArray(int userTestArrayIndex){
