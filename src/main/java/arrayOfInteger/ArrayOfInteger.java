f15907c482249b0f9346e496c62fa311d282f30b
deepmud <73426734+deepmud@users.noreply.github.com>
2021-04-29 17:59:41 +0100

    Removed factorial and refactored the code to

--- src/main/java/arrayOfInteger/ArrayOfInteger.java
+++ src/main/java/arrayOfInteger/ArrayOfInteger.java
@@ -4,22 +4,35 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayOfInteger {
+    public ArrayOfInteger(){
+        System.out.println("This is an example of the project RETURN-THE-MINIMUM-MISSING" +
+                "-NUMBER-IN-ARRAY E.g of Array is {8,1,2,7,3,5,6,10} ");
+        System.out.print("The missing number is:  ");
+    }
+
 
     public int returnMinimumMissingInteger(){
-        int b = 8;
-        int[] n = {8,1,2,7,3,5,6,10};
-        int[] s = new int[8];
+        int b = 15;
+        int[] n = {8,1,2,57,34,33,43,56,22,7,3,9,5,6,10};
+        int[] s = new int[15];
+        for (int i = 0; i < b; i++) {
+            s[i] = i + 1;
+        }
         Arrays.sort(n);
         return CheckMissingInteger( b, s, n);
     }
 
+
     public void returnMinimumMissingIntegerInUserTestArray(int userTestArrayIndex){
-        int b = userTestArrayIndex - 1;
+        int b = userTestArrayIndex;
         int[] n = new int[b];
         int[] s = new int[b];
+        for (int i = 0; i < b; i++) {
+            s[i] = i + 1;
+        }
         System.out.print("Your Unsorted testArray: ");
         for(int i=0;i < b;i++) {
-            n[i] = generateRandomNumber_and_AvoidRepeatedNumberInArray(i,n);
+            n[i] = generateRandomNumber_and_AvoidRepeatedNumberInArrays(i,n,s);
             System.out.print(n[i] + ", ");
         }
         Arrays.sort(n);
@@ -31,11 +44,37 @@ public class ArrayOfInteger {
 
     }
 
-    public int generateRandomNumber_and_AvoidRepeatedNumberInArray(int i, int[] n) {
+    public void printExample1(){
+        System.out.println();
+        System.out.println("This will generate random elements in your array");
+        System.out.println("NB: Your Index mustn't be (>10)");
+        System.out.print("Type in the Index of Array you are interested to test:_");
+    }
+    public int generateRandomNumber_and_AvoidRepeatedNumberInArrays(int i, int[] n,
+                                                                    int[] s) {
         int d = ((int) (Math.random() * 10)) + 1;
+        int g = ((int) (Math.random() * 10)) + 1;
+        int p = ((int) (Math.random() * 100)) + 1;
         for(int k = i-1;k >= 0;k--){
             if(n[k] == d){
-                d = generateRandomNumber_and_AvoidRepeatedNumberInArray(i,n);
+                    d = s[s.length - 1 - g];
+                    for(int l = i - 1;l >= 0; l--){
+                        if(n[l] == d) {
+                            d = s[s.length - 1] + g;
+                            for(int y = i - 1;y >= 0; y--){
+                                if(n[y] == d) {
+                                    d = s[s.length - 1] + g + g;
+                                    for(int z = i - 1;z >= 0; z--) {
+                                        if (n[z] == d) {
+                                            d = s[s.length - 1] + g + g + p;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
             }
         }
         return d;
@@ -43,7 +82,6 @@ public class ArrayOfInteger {
 
     public int CheckMissingInteger(int b,int[] s,int[] n) {
         for (int i = 0; i < b; i++) {
-            s[i] = i + 1;
             if (n[i] != s[i]) {
                 System.out.println("testArray do not have number:  " + s[i]);
                 return s[i];
@@ -55,15 +93,8 @@ public class ArrayOfInteger {
 
     public static void main(String[] args){
         ArrayOfInteger arrayOfInteger = new ArrayOfInteger();
-        System.out.println("This is an example of the project RETURN-THE-MINIMUM-MISSING" +
-                "-NUMBER-IN-ARRAY E.g of Array is {8,1,2,7,3,5,6,10} ");
-        System.out.print("The missing number is:  ");
         arrayOfInteger.returnMinimumMissingInteger();
-
-        System.out.println();
-        System.out.println("This will generate random elements in your array");
-        System.out.println("NB: Your Index mustn't be (>10)");
-        System.out.print("Type in the Index of Array you are interested to test:_");
+        arrayOfInteger. printExample1();
         Scanner input = new Scanner(System.in);
        int userTestArrayIndex = input.nextInt();
       arrayOfInteger.returnMinimumMissingIntegerInUserTestArray(userTestArrayIndex);
